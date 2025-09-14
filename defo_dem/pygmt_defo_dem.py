8ad0a9fdff9b33b8cd2d48e674c6108639c1890b
inzisarshaung <csuyiluntan@gmail.com>
2025-09-14 21:30:35 +0800

    delete the data SIGMA.txt

--- defo_dem/pygmt_defo_dem.py
+++ defo_dem/pygmt_defo_dem.py
@@ -11,12 +11,10 @@ from pygmt_plotter import PyGMTPlotter
 # 形变速率数据
 tif_sbas_allregion = "data/SBAS_AllRegion.tif"
 tif_sbas = "data/SBAS.tif"
-txt_sigma = "data/SIGMA.txt"
 txt_psi = "data/PSI.txt"
 # 转换后的 grd 数据文件
 grd_sbas_allregion = "SBAS_AllRegion.grd"
 grd_sbas = "SBAS.grd"
-grd_sigma = "SIGMA.grd"
 grd_psi = "PSI.grd"
 
 # TIF -> GRD
@@ -24,7 +22,6 @@ region_all  = PyGMTPlotter.load_tif_to_grd(tif_path=tif_sbas_allregion, grd_path
 region_sbas = PyGMTPlotter.load_tif_to_grd(tif_path=tif_sbas, grd_path=grd_sbas)
 
 # TXT -> GRD
-region_sigma = PyGMTPlotter.load_txt_to_grd(txt_path=txt_sigma, grd_path=grd_sigma, space=0.0005)
 region_psi   = PyGMTPlotter.load_txt_to_grd(txt_path=txt_psi, grd_path=grd_psi, space=0.0005)
 
 # ===================== 2) 初始化绘图器 & 设置默认样式 =====================
@@ -47,7 +44,6 @@ cpt_saga = "data/saga-01.cpt"
 (Path(__file__).resolve().parent / "result").mkdir(parents=True, exist_ok=True)
 output_sbas_allregion = "result/SBAS_AllRegion.png"
 output_sbas = "result/SBAS.png"
-output_sigma = "result/SIGMA.png"
 output_psi = "result/PSI.png"
 
 # ===================== 4) 出图（DEM 底图 + 形变叠加 + 指北针 + 比例尺 + 色标） =====================
@@ -96,30 +92,6 @@ region_sbas = plotter.region_from_grid(grd_sbas)
     .save(output_sbas)
 )
 
-# 修改全局的默认绘图参数
-plotter.set_defaults(
-    FONT_TITLE="28p,Helvetica-Bold",     # 标题更大
-    FONT_ANNOT_PRIMARY="14p,Helvetica-Bold",  # 轴注/比例尺刻度更大
-    FONT_LABEL="20p,Helvetica-Bold",
-)
-region_sigma = plotter.region_from_grid(grd_sigma)
-(plotter
-    .new() # 应用新的默认样式
-    .draw_basemap(region=region_sigma, projection=projection, title="Deformation: SIGMA")
-    .draw_dem(dem_tif=tif_dem, region=region_sigma, projection=projection)
-    .draw_deformation(
-        data_grd=grd_sigma,
-        cpt=cpt_saga,
-        region=region_sigma,
-        projection=projection,
-        bar_min=-0.06, bar_max=0.06,
-        alpha=25
-    )
-    .add_colorbar_bottom()
-    .add_scale_top(region=region_sigma, projection=projection)
-    .add_rose_right(region=region_sigma, projection=projection)
-    .save(output_sigma)
-)
 
 # 恢复原有的默认绘图参数
 plotter.reset_defaults() # 又回到初始默认样式
